3242532c5c1581851b570853b5354e7a924e9ca9
edo-2313 <32812884+edo2313@users.noreply.github.com>
2021-12-02 16:34:52 +0100

    day2

---
+++ day2/main.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+var array = fs.readFileSync('input.txt').toString().split("\n");
+
+//Part 1
+console.log("Part 1 result: ")
+
+let hpos = 0;
+let depth = 0;
+for (let i = 0; i < array.length; i++) {
+    let command = array[i].split(" ");
+    if (command[0] == "forward") {
+        hpos += parseInt(command[1]);
+    }
+    if (command[0] == "up") {
+        depth -= parseInt(command[1]);
+    }
+    if (command[0] == "down") {
+        depth += parseInt(command[1]);
+    }
+}
+console.log(hpos * depth);
+
+//Part 2
+console.log("Part 2 result: ")
+
+hpos = 0;
+depth = 0;
+let aim = 0;
+for (let i = 0; i < array.length; i++) {
+    let command = array[i].split(" ");
+    if (command[0] == "forward") {
+        hpos += parseInt(command[1]);
+        depth += aim*parseInt(command[1]);
+    }
+    if (command[0] == "up") {
+        aim -= parseInt(command[1]);
+    }
+    if (command[0] == "down") {
+        aim += parseInt(command[1]);
+    }
+}
+console.log(hpos * depth);
